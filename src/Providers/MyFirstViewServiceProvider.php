2b8c7bdc3c09a91e98eb339c21e95187daf7b025
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-15 13:46:24 +0100

    First Backend view

---
+++ src/Providers/MyFirstViewServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MyFirstView\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class MyFirstViewServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+
+	}
+}
+
+?>
